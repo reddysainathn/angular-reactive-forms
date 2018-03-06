8fcfa76bee41839c6accc5b9d879fef3b1c95d5b
Sainath <10643404+reddysainathn@users.noreply.github.com>
2018-03-05 18:27:36 -0600

    Bootstrap Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { SignupFormComponent } from './components/signup-form/signup-form.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupFormComponent
   ],
   imports: [
     BrowserModule,
