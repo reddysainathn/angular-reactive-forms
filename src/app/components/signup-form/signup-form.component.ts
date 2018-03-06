cc29f5bd52e7dc3fdadb9236ccdb97b6558a0ac7
Sainath <10643404+reddysainathn@users.noreply.github.com>
2018-03-05 19:29:03 -0600

    Adding Form Validation on Submit

--- src/app/components/signup-form/signup-form.component.ts
+++ src/app/components/signup-form/signup-form.component.ts
@@ -22,4 +22,10 @@ export class SignupFormComponent {
   get username() {
     return this.form.get('username')
   }
+
+  login(){
+    this.form.setErrors({
+      invalidLogin: true
+    });
+  }
 }
